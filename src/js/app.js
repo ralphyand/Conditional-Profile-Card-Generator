5d28bebce38e58cdb49a3d5920e5e5f27b5a5379
ralphyand <MOISELK211@GMAIL.COM>
2022-06-06 15:10:10 +0000

    correcion null en link

--- src/js/app.js
+++ src/js/app.js
@@ -45,19 +45,20 @@ function render(variables = {}) {
                   ? variables.socialMediaPosition
                   : ""
               }">
+
                 <li><a href="https://twitter.com/${
-                  variables.twitter
+                  variables.twitter ? variables.twitter : ""
                 }"><i class="fab fa-twitter"></i></a></li>
                 <li><a href="https://github.com/${
-                  variables.github
+                  variables.github ? variables.github : ""
                 }"><i class="fab fa-github"></i></a></li>
                 <li><a href="https://linkedin.com/${
-                  variables.linkedin
+                  variables.linkedin ? variables.linkedin : ""
                 }"><i class="fab fa-linkedin"></i></a></li>
                 <li><a href="https://instagram.com/${
-                  variables.instagram
+                  variables.instagram ? variables.instagram : ""
                 }"><i class="fab fa-instagram"></i></a></li>
-              </ul>
+               </ul>
             </div>
         `;
 }
